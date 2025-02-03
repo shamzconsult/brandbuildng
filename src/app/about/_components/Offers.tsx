92a2e0f8ac27b842263f8865f6f53364073c509a
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2025-02-03 13:51:44 +0100

    Chore: seting up Offer section

--- src/app/about/_components/Offers.tsx
+++ src/app/about/_components/Offers.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Offers() {
   return (
-    <div className='mx-auto max-w-screen-xl p-4'>
+    <div className='mx-auto max-w-screen-xl p-4 mb-16'>
         <h2 className='mb-10 text-3xl md:text-5xl text-gray-500'>Hey Friend, Check out our Offer!</h2>
         <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
 
@@ -39,17 +39,17 @@ function Offers() {
                 <span
                     className="absolute -right-px -top-px rounded-bl-3xl rounded-tr-3xl bg-orange-500 px-6 py-4 font-medium uppercase tracking-widest text-white"
                 >
-                    Save 10%
+                    Save 11%
                 </span>
 
                 <img
-                    src="./assets/images/Offer-image/packaged-item.JPG"
+                    src="./assets/images/Offer-image/Event-Branding.jpg"
                     alt="Packaged-item"
                     className="h-80 w-full rounded-tr-3xl object-cover"
                 />
 
                 <div className="p-4 text-center">
-                    <strong className="text-xl font-medium text-gray-900"> Gift-items </strong>
+                    <strong className="text-xl font-medium text-gray-900"> Event Branding </strong>
 
                     <p className="mt-2 text-pretty text-gray-700">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia rem vel voluptatum in
@@ -68,17 +68,17 @@ function Offers() {
                 <span
                     className="absolute -right-px -top-px rounded-bl-3xl rounded-tr-3xl bg-orange-500 px-6 py-4 font-medium uppercase tracking-widest text-white"
                 >
-                    Save 10%
+                    Save 9%
                 </span>
 
                 <img
-                    src="./assets/images/Offer-image/packaged-item.JPG"
+                    src="./assets/images/Offer-image/plastic-bag.jpg"
                     alt="Packaged-item"
                     className="h-80 w-full rounded-tr-3xl object-cover"
                 />
 
                 <div className="p-4 text-center">
-                    <strong className="text-xl font-medium text-gray-900"> Gift-items </strong>
+                    <strong className="text-xl font-medium text-gray-900"> Zip-lock bags </strong>
 
                     <p className="mt-2 text-pretty text-gray-700">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia rem vel voluptatum in
@@ -97,17 +97,17 @@ function Offers() {
                 <span
                     className="absolute -right-px -top-px rounded-bl-3xl rounded-tr-3xl bg-orange-500 px-6 py-4 font-medium uppercase tracking-widest text-white"
                 >
-                    Save 10%
+                    Save 12%
                 </span>
 
                 <img
-                    src="./assets/images/Offer-image/packaged-item.JPG"
+                    src="./assets/images/Offer-image/Content-creation-on-social-media.jpg"
                     alt="Packaged-item"
                     className="h-80 w-full rounded-tr-3xl object-cover"
                 />
 
                 <div className="p-4 text-center">
-                    <strong className="text-xl font-medium text-gray-900"> Gift-items </strong>
+                    <strong className="text-xl font-medium text-gray-900"> Content Creation </strong>
 
                     <p className="mt-2 text-pretty text-gray-700">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia rem vel voluptatum in
@@ -126,17 +126,17 @@ function Offers() {
                 <span
                     className="absolute -right-px -top-px rounded-bl-3xl rounded-tr-3xl bg-orange-500 px-6 py-4 font-medium uppercase tracking-widest text-white"
                 >
-                    Save 10%
+                    Save 3%
                 </span>
 
                 <img
-                    src="./assets/images/Offer-image/packaged-item.JPG"
+                    src="./assets/images/Offer-image/transparent-bags.jpg"
                     alt="Packaged-item"
                     className="h-80 w-full rounded-tr-3xl object-cover"
                 />
 
                 <div className="p-4 text-center">
-                    <strong className="text-xl font-medium text-gray-900"> Gift-items </strong>
+                    <strong className="text-xl font-medium text-gray-900"> Waterproof transparent bags </strong>
 
                     <p className="mt-2 text-pretty text-gray-700">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia rem vel voluptatum in
@@ -155,17 +155,17 @@ function Offers() {
                 <span
                     className="absolute -right-px -top-px rounded-bl-3xl rounded-tr-3xl bg-orange-500 px-6 py-4 font-medium uppercase tracking-widest text-white"
                 >
-                    Save 10%
+                    Save 8%
                 </span>
 
                 <img
-                    src="./assets/images/Offer-image/packaged-item.JPG"
+                    src="./assets/images/Offer-image/shirt.jpg"
                     alt="Packaged-item"
                     className="h-80 w-full rounded-tr-3xl object-cover"
                 />
 
                 <div className="p-4 text-center">
-                    <strong className="text-xl font-medium text-gray-900"> Gift-items </strong>
+                    <strong className="text-xl font-medium text-gray-900"> Branded Shirt </strong>
 
                     <p className="mt-2 text-pretty text-gray-700">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia rem vel voluptatum in
@@ -179,6 +179,94 @@ function Offers() {
                     </span>
                 </div>
             </div>
+
+            <div className="relative block rounded-tr-3xl border bg-slate-50 shadow-md rounded-2xl border-gray-100">
+                <span
+                    className="absolute -right-px -top-px rounded-bl-3xl rounded-tr-3xl bg-orange-500 px-6 py-4 font-medium uppercase tracking-widest text-white"
+                >
+                    Save 8%
+                </span>
+
+                <img
+                    src="./assets/images/Offer-image/branded-umbrella.jpg"
+                    alt="Packaged-item"
+                    className="h-80 w-full rounded-tr-3xl object-cover"
+                />
+
+                <div className="p-4 text-center">
+                    <strong className="text-xl font-medium text-gray-900"> Max-size Branded Umbrella </strong>
+
+                    <p className="mt-2 text-pretty text-gray-700">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia rem vel voluptatum in
+                    eum vitae aliquid at sed dignissimos.
+                    </p>
+
+                    <span
+                    className="mt-4 block rounded-md border border-orange-900 bg-orange-500 px-5 py-3 text-sm font-medium uppercase tracking-widest text-white transition-colors"
+                    >
+                    #20,000 NGN
+                    </span>
+                </div>
+            </div>
+
+            <div className="relative block rounded-tr-3xl border bg-slate-50 shadow-md rounded-2xl border-gray-100">
+                <span
+                    className="absolute -right-px -top-px rounded-bl-3xl rounded-tr-3xl bg-orange-500 px-6 py-4 font-medium uppercase tracking-widest text-white"
+                >
+                    Save 8%
+                </span>
+
+                <img
+                    src="./assets/images/Offer-image/best-digital-marketing.webp"
+                    alt="Packaged-item"
+                    className="h-80 w-full rounded-tr-3xl object-cover"
+                />
+
+                <div className="p-4 text-center">
+                    <strong className="text-xl font-medium text-gray-900"> Digital Marketing </strong>
+
+                    <p className="mt-2 text-pretty text-gray-700">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia rem vel voluptatum in
+                    eum vitae aliquid at sed dignissimos.
+                    </p>
+
+                    <span
+                    className="mt-4 block rounded-md border border-orange-900 bg-orange-500 px-5 py-3 text-sm font-medium uppercase tracking-widest text-white transition-colors"
+                    >
+                    #20,000 NGN
+                    </span>
+                </div>
+            </div>
+
+            <div className="relative block rounded-tr-3xl border bg-slate-50 shadow-md rounded-2xl border-gray-100">
+                <span
+                    className="absolute -right-px -top-px rounded-bl-3xl rounded-tr-3xl bg-orange-500 px-6 py-4 font-medium uppercase tracking-widest text-white"
+                >
+                    Save 8%
+                </span>
+
+                <img
+                    src="./assets/images/Offer-image/kappframework-Jsgnic(1)(1).png"
+                    alt="Packaged-item"
+                    className="h-80 w-full rounded-tr-3xl object-cover"
+                />
+
+                <div className="p-4 text-center">
+                    <strong className="text-xl font-medium text-gray-900"> Brand Banner Customization </strong>
+
+                    <p className="mt-2 text-pretty text-gray-700">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia rem vel voluptatum in
+                    eum vitae aliquid at sed dignissimos.
+                    </p>
+
+                    <span
+                    className="mt-4 block rounded-md border border-orange-900 bg-orange-500 px-5 py-3 text-sm font-medium uppercase tracking-widest text-white transition-colors"
+                    >
+                    #20,000 NGN
+                    </span>
+                </div>
+            </div>
+
         </div>
     </div>
   )
